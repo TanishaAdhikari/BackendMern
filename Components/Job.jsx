a907414d9656fd46e150f9d9cefd02bbe7bb005c
_taneesha_21 <itschhaya19k@gmail.com>
2026-07-14 22:26:10 +0545

    Add files via upload

---
+++ Components/Job.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Job = () => {
+  return (
+    <div>
+      <h2>Job Opportunities</h2>
+      <p>Check out our available positions!</p>
+    </div>
+  )
+}
+
+export default Job
---
+++ Components/Product.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <div>
+      <h2>Our Products</h2>
+      <p>Check out our amazing products!</p>
+    </div>
+  )
+}
+
+export default Product
